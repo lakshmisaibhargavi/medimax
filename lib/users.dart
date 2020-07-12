5347c59ea8118a9e2d0ef458f7d158ef0606381b
lakshmisaibhargavi <55940136+lakshmisaibhargavi@users.noreply.github.com>
2020-07-12 17:44:51 +0530

    Delete users.dart

--- lib/users.dart
+++
@@ -1,8 +0,0 @@
-const mockUsers = const {
-  'divignanesh@gmail.com': 'medica',
-  'g.lakshmisb@gmail.com': 'medica',
-  'arpith@gmail.com': 'medica',
-  'naganitinsydney@gmail.com': 'medica',
-  'pulkit@gmail.com': 'medica',
-  '@.com': '.',
-};
