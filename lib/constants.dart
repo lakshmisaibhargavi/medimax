16c1fdd41035ed3e986bfe487e819df389d40e69
lakshmisaibhargavi <55940136+lakshmisaibhargavi@users.noreply.github.com>
2020-07-12 17:44:06 +0530

    Delete constants.dart

--- lib/constants.dart
+++
@@ -1,5 +0,0 @@
-class Constants {
-  static const String appName = 'Mente_medica';
-  static const String logoTag = 'near.huscarl.loginsample.logo';
-  static const String titleTag = 'in2_urslf';
-}
